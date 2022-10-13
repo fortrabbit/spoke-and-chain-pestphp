efc84569c2f54a5a45c9a3335773e54139adb7e5
Oliver Stark <os@fortrabbit.com>
2022-10-13 13:37:51 +0200

    Pest: Full multi step process (not passing so far)

--- tests/Feature/CheckoutTest.php
+++ tests/Feature/CheckoutTest.php
@@ -1,87 +1,65 @@
 <?php
 
-test('checkout as a guest', function() {
+test('full checkout process as a guest', function($newUser) {
 
+    // 1. Add a product to the cart
     $this->get('/product/san-quentin-24')
-        ->assertOk()
         ->form('#buy form')
         ->submit()
-        ->assertOk();
+        ->assertStatus(200);
 
-});
+    // 2.Checkout as guest using email
+    $next = $this->get('/checkout')
+        ->assertStatus(200)
+        ->form('#guest-checkout')
+        ->fill('email', $newUser['email'])
+        ->submit()
+        ->assertRedirectTo('/checkout/address');
 
-/*
+    // 3. Address form
+    $next = $next
+        ->form('#checkout-address')
+        ->fill('firstName', $newUser['firstName'])
+        ->fill('lastName', $newUser['lastName'])
+        ->fill('addressLine1', $newUser['addressLine1'])
+        ->fill('locality', $newUser['locality'])
+        ->fill('postalCode', $newUser['postalCode'])
+        ->select('countryCode', $newUser['contry'])
+        ->submit()
+        ->assertRedirectTo('/checkout/shipping');
 
-namespace crafttests\acceptance;
-use Craft;
-use AcceptanceTester;
+    // 4. Shipping form
+    $next = $next
+        ->form('#checkout-shipping')
+        ->tick('shippingMethodHandle', 'freeShipping')
+        ->submit()
+        ->assertRedirectTo('/checkout/summary');
 
-class CheckoutCest
-{
+    // 5. Payment form
+    $next = $next
+        ->form('#checkout-payment')
+        ->fill('paymentForm[dummy][firstName]', $newUser['firstName'])
+        ->fill('paymentForm[dummy][lastName]', $newUser['lastName'])
+        ->fill('paymentForm[dummy][number]', $newUser['cardNumber'])
+        ->fill('paymentForm[dummy][expiry]', $newUser['cardExpiry'])
+        ->fill('paymentForm[dummy][cvv]', $newUser['cardCvv'])
+        ->submit()
+        ->assertRedirectTo('/checkout/success?number=*');
 
-public
-function testCheckout(AcceptanceTester $I, \Codeception\Example $singleUser)
-{
-    // Add a product to the cart
-    $I->amOnPage('product/san-quentin-24');
-    $I->click('#buy button[type=submit]');
+    // 6. Finally
+    $next
+        ->querySelector('h1')
+        ->assertText('Success');
 
-    // Navigate to the cart
-    $I->click('button.cart-toggle');
-    $I->see('Check Out', 'div.cart-menu a.button.submit');
-    $I->click('div.cart-menu a.button.submit');
-
-    // Checkout as guest
-    $I->see('Continue as Guest', '#guest-checkout button[type=submit]');
-    $I->fillField('#guest-checkout input[type=text]', $singleUser['email']);
-    $I->click('#guest-checkout button[type=submit]');
-
-    $I->submitForm('form#checkout-address', ['shippingAddress' => [
-        'firstName' => $singleUser['firstName'],
-        'lastName' => $singleUser['lastName'],
-        'addressLine1' => $singleUser['addressLine1'],
-        'locality' => $singleUser['locality'],
-        'postalCode' => $singleUser['postalCode'],
-        'countryCode' => $singleUser['countryCode'],
-    ]]);
-    $I->amOnPage('/checkout/shipping');
-
-    // Use the default shipping method
-    $I->click('form#checkout-shipping-method input[type=radio][value="freeShipping"]');
-    $I->click('form#checkout-shipping-method button[type=submit]');
-
-    // Fill credit card details and pay
-    $I->submitForm('form#checkout-payment', ['paymentForm' => [
-        'dummy' => [
-            'firstName' => $singleUser['firstName'],
-            'lastName' => $singleUser['lastName'],
-            'number' => $singleUser['cardNumber'],
-            'expiry' => $singleUser['cardExpiry'],
-            'cvv' => $singleUser['cardCvv'],
-        ]
-    ]]);
-
-    // success
-    $I->see('Success', 'h1');
-}
-
-protected
-function userProvider()
-{
-    return [
-        [
-            'email' => 'ben@craftcms.com',
-            'firstName' => 'Ben',
-            'lastName' => 'David',
-            'addressLine1' => '13 rue des Papillons',
-            'locality' => 'Grenoble',
-            'postalCode' => '38000',
-            'countryCode' => 'FR',
-            'cardNumber' => '4242424242424242',
-            'cardExpiry' => '03/2026',
-            'cardCvv' => '123',
-        ],
-    ];
-}
-}
-*/
+})->with([
+    'email' => 'ben@craftcms.com',
+    'firstName' => 'Ben',
+    'lastName' => 'David',
+    'addressLine1' => '13 rue des Papillons',
+    'locality' => 'Grenoble',
+    'postalCode' => '38000',
+    'countryCode' => 'FR',
+    'cardNumber' => '4242424242424242',
+    'cardExpiry' => '03/2026',
+    'cardCvv' => '123',
+]);
